b6e1cba6bee97b7643151db509a68d3bb27db218
Imran3232 <33367678+Imran3232@users.noreply.github.com>
2017-11-04 13:51:41 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Assignment Submitter</title>
+    </head>
+    <body>
+        <?php
+        include 'Welcome_page.php';
+        ?>
+    </body>
+</html>
